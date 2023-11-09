3d94eb6c5607b29e2bd8b5c8ec7662313eddf977
Topher05L <89708367+Topher05@users.noreply.github.com>
2023-11-09 17:14:54 -0500

    prints both first page and second page issue #5

--- youtube.py
+++ youtube.py
@@ -11,24 +11,32 @@ def youtube_search(query_term, max_results):
 	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
 					  developerKey=DEVELOPER_KEY)
 	
-	# Call the search.list method to retrieve results matching the specified
-	#query term.
-	search_response = youtube.search().list(
+	#so we can get next page token
+	search_response1 = youtube.search().list(
 		q=query_term,
 		part='id,snippet',
 		maxResults=max_results,
 	).execute()
 
+	# Call the search.list method to retrieve results matching the specified
+	#query term.
+	search_response2 = youtube.search().list(
+		q=query_term,
+		part='id,snippet',
+		maxResults=max_results,
+		pageToken= search_response1.get('nextPageToken'),
+	).execute()
+
 	#added this to make it a little more readable
 	videos = []
 
-	for search_result in search_response.get('items', []):
+	for search_result in search_response1.get('items', []):
 		if search_result['id']['kind'] == 'youtube#video':
 			videos.append('%s (%s)' % (search_result['snippet']['title'], 
 										search_result['id']['videoId']))
 	
 	print('Videos:\n', '\n'.join(videos), '\n')
-	return search_response.get('items', [])
+	return search_response1.get('items', []), search_response2.get('items', [])
 
 
 if __name__ == "__main__":

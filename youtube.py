63cb178cf4cefd88d0266dfc1d3cbf31c32f5734
Topher05L <89708367+Topher05@users.noreply.github.com>
2023-11-09 20:03:24 -0500

    allows for a third parameter to chose a specific page. issue #6

--- youtube.py
+++ youtube.py
@@ -7,9 +7,10 @@ DEVELOPER_KEY = config.API_KEY
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def youtube_search(query_term, max_results):
+def youtube_search(query_term, max_results, pageNum):
 	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
 					  developerKey=DEVELOPER_KEY)
+	
 	
 	#so we can get next page token
 	search_response1 = youtube.search().list(
@@ -18,31 +19,28 @@ def youtube_search(query_term, max_results):
 		maxResults=max_results,
 	).execute()
 
-	# Call the search.list method to retrieve results matching the specified
-	#query term.
-	search_response2 = youtube.search().list(
-		q=query_term,
-		part='id,snippet',
-		maxResults=max_results,
-		pageToken= search_response1.get('nextPageToken'),
-	).execute()
+	page = search_response1
+	while pageNum > 0:
+		# Call the search.list method to retrieve results matching the specified
+		#query term.
+		search_response2 = youtube.search().list(
+			q=query_term,
+			part='id,snippet',
+			maxResults=max_results,
+			pageToken= page.get('nextPageToken'),
+		).execute()
+		
+		page = search_response2
+		pageNum -=1
 
-	#added this to make it a little more readable
-	videos = []
-
-	for search_result in search_response1.get('items', []):
-		if search_result['id']['kind'] == 'youtube#video':
-			videos.append('%s (%s)' % (search_result['snippet']['title'], 
-										search_result['id']['videoId']))
-	
-	print('Videos:\n', '\n'.join(videos), '\n')
-	return search_response1.get('items', []), search_response2.get('items', [])
+	return page.get('items', [])
 
 
 if __name__ == "__main__":
 	query_term = sys.argv[1]
 	max_results = sys.argv[2]
+	pageNum = sys.argv[3]
 	try:
-		print(youtube_search(query_term, max_results))
+		print(youtube_search(query_term, max_results, int(pageNum)))
 	except HttpError as e:
 		print('An HTTP error %d occurred:\n%s' % (type(e).__name__, str(e)))
